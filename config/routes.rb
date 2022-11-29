364254b6dab53c23acee2b7d732e2d2887439ae6
Guangxin Yu <1572250276@qq.com>
2022-11-29 17:50:31 +0800

    added routes for login

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   namespace :api, default: { format: :json } do
     namespace :v1 do
+      post 'login', to: 'sessions#login', as: :login
       resources :shows, only: %i[index]
     end
   end
