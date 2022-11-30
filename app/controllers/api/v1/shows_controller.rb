53ea3037ade70350b2dafb9b07a292e5780b6218
Guangxin Yu <1572250276@qq.com>
2022-11-30 12:25:31 +0800

    added search condition into shows#index

--- app/controllers/api/v1/shows_controller.rb
+++ app/controllers/api/v1/shows_controller.rb
@@ -1,6 +1,17 @@
 class Api::V1::ShowsController < Api::V1::BaseController
   def index
-    @shows = Show.all
+    if params[:query]
+      sql_query = <<~SQL
+        name ILIKE :query
+        OR description ILIKE :query
+        OR address ILIKE :query
+        OR comedian ILIKE :query
+      SQL
+      @shows = Show.where(sql_query, query: "%#{params[:query]}%")
+    else
+      @shows = Show.all
+    end
+
     render json: { shows: @shows }
   end
 
