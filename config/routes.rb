48970e7ded5d612931ec811adfc27603fc7ccb1d
Guangxin Yu <1572250276@qq.com>
2022-11-30 18:24:56 +0800

    added shows#destroy

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   namespace :api, default: { format: :json } do
     namespace :v1 do
       post 'login', to: 'sessions#login', as: :login
-      resources :shows, only: %i[index show create]
+      resources :shows, only: %i[index show create destroy]
       resources :users, only: %i[show update]
     end
   end
