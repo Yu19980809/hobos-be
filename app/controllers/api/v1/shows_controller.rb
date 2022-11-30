b30c056d14a0a13e32affb4d46932c67556b7aa3
Guangxin Yu <1572250276@qq.com>
2022-11-30 10:48:40 +0800

    added shows#show

--- app/controllers/api/v1/shows_controller.rb
+++ app/controllers/api/v1/shows_controller.rb
@@ -3,4 +3,9 @@ class Api::V1::ShowsController < Api::V1::BaseController
     @shows = Show.all
     render json: { shows: @shows }
   end
+
+  def show
+    @show = Show.find(params[:id])
+    render json: { show: @show }
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   namespace :api, default: { format: :json } do
     namespace :v1 do
       post 'login', to: 'sessions#login', as: :login
-      resources :shows, only: %i[index]
+      resources :shows, only: %i[index show]
     end
   end
 end
