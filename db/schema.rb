982cbe286e65374d3b164236013501bea3cda826
Guangxin Yu <1572250276@qq.com>
2022-11-30 18:35:08 +0800

    generated model booking

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_29_092457) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_30_103425) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.bigint "show_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["show_id"], name: "index_bookings_on_show_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
 
   create_table "shows", force: :cascade do |t|
     t.string "name"
@@ -36,5 +45,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_29_092457) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "bookings", "shows"
+  add_foreign_key "bookings", "users"
   add_foreign_key "shows", "users"
 end
