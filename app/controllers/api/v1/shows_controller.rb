749610febe40ecf69560d5370f88e4fdee071bdf
Guangxin Yu <1572250276@qq.com>
2022-11-30 11:12:36 +0800

    added shows#create

--- app/controllers/api/v1/shows_controller.rb
+++ app/controllers/api/v1/shows_controller.rb
@@ -8,4 +8,21 @@ class Api::V1::ShowsController < Api::V1::BaseController
     @show = Show.find(params[:id])
     render json: { show: @show }
   end
+
+  def create
+    @show = Show.new(show_params)
+    @show.user = User.find(params[:user_id])
+
+    if @show.save
+      render json: { show: @show }
+    else
+      render json: @show.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def show_params
+    params.require(:show).permit(:name, :description, :poster_url, :address, :time, :comedian)
+  end
 end
